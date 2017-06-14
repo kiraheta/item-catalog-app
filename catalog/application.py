82ee858f0a071797b23cb228295e30c4a627a874
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-06-14 15:59:49 -0700

    Add starting files

---
+++ catalog/application.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+"""
+This program is a web application that provides a list of items within a
+variety of categories and allows Authenticated users to post, edit, and delete
+their own items.
+
+@author: Kenny Iraheta
+@Date: 2017-06-14
+"""
