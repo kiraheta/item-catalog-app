8915c0f1fea2941ddf159112f15a1037d2b07385
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-06-14 20:04:14 -0700

    Setup up routing decorators

--- catalog/application.py
+++ catalog/application.py
@@ -2,7 +2,7 @@
 
 """
 This program is a web application that provides a list of items within a
-variety of categories and allows Authenticated users to post, edit, and delete
+variety of categories and allows authenticated users to post, edit, and delete
 their own items.
 
 @author: Kenny Iraheta
@@ -14,7 +14,7 @@ from flask import Flask, render_template, request, redirect, jsonify, url_for, f
 
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Categories, CategoryItem, User
+from database_setup import Base, Category, CategoryItem, User
 
 from flask import session as login_session
 import random
@@ -36,6 +36,57 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+
+@app.route('/')
+@app.route('/category/')
+def showCategories():
+    """Show all categories"""
+    categories = session.query(Category).order_by(asc(Category.name))
+    return "Hello World!"
+
+
+@app.route('/category/new/', methods=['GET', 'POST'])
+def newCategory():
+    """Create new category"""
+    return "Create new category"
+
+
+@app.route('/category/<int:category_id>/edit/', methods=['GET', 'POST'])
+def editCategory(category_id):
+    """Edit new category"""
+    return "Edit category"
+
+
+# Delete a category
+@app.route('/category/<int:category_id>/delete/', methods=['GET', 'POST'])
+def deleteCategory(category_id):
+    """Delete a category"""
+    return "Delete a category"
+
+
+@app.route('/category/<int:category_id>/<int:categoryItem_id>')
+def showCategoryItem(category_id,categoryItem_id):
+    """Show a category item"""
+    return "Show a category item"
+
+
+@app.route('/category/<int:category_id>/new/', methods=['GET', 'POST'])
+def newCategoryItem(category_id):
+    """Create a category item"""
+    return "Create a category item"
+
+
+@app.route('/category/<int:category_id>/<int:categoryItem_id>/edit', methods=['GET', 'POST'])
+def editCategoryItem(category_id, categoryItem_id):
+    """Edit a category item"""
+    return "Edit a category item"
+
+
+@app.route('/category/<int:category_id>/<int:categoryItem_id>/delete', methods=['GET', 'POST'])
+def deleteCategoryItem(category_id, categoryItem_id):
+    """Delete a category item"""
+    return "Delete a category item"
 
 
 if __name__ == '__main__':
