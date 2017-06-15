3a4d90d1efcf7c999598bea96c08336456904154
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-06-14 17:03:12 -0700

    Add tables

--- catalog/database_setup.py
+++ catalog/database_setup.py
@@ -5,7 +5,38 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
-engine = create_engine('sqlite:///restaurantmenu.db')
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Categories(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+class CategoryItem(Base):
+    __tablename__ = 'category_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Categories)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+engine = create_engine('sqlite:///categoryitems.db')
 
 
 Base.metadata.create_all(engine)
