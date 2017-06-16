889bde3eb1e783d59ab12e1495afcb8dc67252a4
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-06-15 21:01:24 -0700

    Update database_setup

--- catalog/database_setup.py
+++ catalog/database_setup.py
@@ -20,16 +20,16 @@ class Category(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    # user_id = Column(Integer, ForeignKey('user.id'))
+    # user = relationship(User)
 
     @property
     def serialize(self):
-        """Return object data in serializeable format"""
+        """Return object data in easily serializeable format"""
         return {
             'name': self.name,
             'id': self.id,
-            }
+        }
 
 
 class CategoryItem(Base):
@@ -40,17 +40,18 @@ class CategoryItem(Base):
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship(User)
+    # user_id = Column(Integer, ForeignKey('user.id'))
+    # user = relationship(User)
 
     @property
     def serialize(self):
-        """Return object data in serializeable format"""
+        """Return object data in easily serializeable format"""
         return {
             'name': self.name,
             'description': self.description,
             'id': self.id,
-            }
+        }
+
 
 engine = create_engine('sqlite:///categoryitems.db')
 
