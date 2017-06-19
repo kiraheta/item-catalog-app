d701e0d2dddbd8816d4a13e3125cf160b5aa74c9
Kenny Iraheta <iraheta.kenny@gmail.com>
2017-06-18 18:55:22 -0700

    Add ON DELETE CASCADE

--- catalog/database_setup.py
+++ catalog/database_setup.py
@@ -2,6 +2,8 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+from sqlalchemy import backref
+
 
 Base = declarative_base()
 
@@ -39,7 +41,7 @@ class CategoryItem(Base):
     id = Column(Integer, primary_key=True)
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
-    category = relationship(Category)
+    category = relationship(Category, backref=backref('catalog_item', cascade='all,delete'))
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
